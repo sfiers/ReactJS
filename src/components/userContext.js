e2988d03fa036102f063e722f143954ec2760b3c
sfiers <simon.fiers@outlook.com>
2021-11-16 12:04:25 +0100

    40 Context (Part 3)

--- src/components/userContext.js
+++ src/components/userContext.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import User from './User'
 
-const UserContext = React.createContext()
+const UserContext = React.createContext('codevolution')
 
 const UserProvider = UserContext.Provider
 const UserConsumer = UserContext.Consumer
 
 export { UserProvider, UserConsumer}
+export default UserContext
